1b42033c0c58d80aca8a806978bcb921f462fb5a
RowanPl <rowanplooij@hotmail.com>
2023-08-15 16:19:02 +0200

    small changes in which request can be done

--- src/components/Docs.jsx
+++ src/components/Docs.jsx
@@ -6,15 +6,12 @@ import "../App.css"; // Import your custom CSS
 
 function Docs(applicationName, updateClick) {
 
-
-
     useEffect(() => {
         swagger.servers[1] = {url: "https://api.datavortex.nl/" + applicationName.applicationName};
         console.log(swagger.servers[1].url)
         console.log(updateClick.updateClick)
     }, [applicationName.applicationName, updateClick.updateClick]);
 
-    SwaggerUI.tryItOutEnabled = false;
 
     return (
         <SwaggerUI spec={swagger}/>
